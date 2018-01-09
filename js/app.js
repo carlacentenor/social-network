647c84e86f818a308509ab92095bdf1cf0f795d5
dayamll <dayamllerenacastro@gmail.com>
2018-01-09 12:06:20 -0500

    subiendo ultimos estilos de splash,login y signup

--- js/app.js
+++ js/app.js
@@ -2,26 +2,5 @@
 $(document).ready(function() {
   setTimeout(function() {
     window.location.href = 'views/register.html';
-  }, 1000);
+  }, 4000);
 });
-
-// efecto input 
-$(document).ready(function() {
-  Materialize.updateTextFields();
-
-
-  // función login
-  $('#btn-login').on('click', function(event) {
-    event.preventDefault();
-    var email = $('.name').val();
-    var password = $('.password').val();
-  
-    localStorage.email = email;
-    localStorage.password = password;
-   
-
-    window.location.href = '../views/home.html';
-  });
-});
-
-
--- js/data.js
+++ js/data.js
@@ -11,20 +11,20 @@ var dataUsers = [
   },
   {
     name: 'Pedro Carrillo',
-    email: 'pedrocarrillo@gmail.com',
+    email: 'carrillopedro158@gmail.com',
     photoUser: '../assets/images/user2.png',
     poster: '../assets/images/portada2.jpg',
-    password: 123456,
+    password: 'pedro12345',
     age: 35,
     contacts: ['lauralopez@gmail.com', 'mariatorres@gmail.com'],
     post: 13
   },
   {
     name: 'Maria Torres',
-    email: 'mariatorres@gmail.com',
+    email: 'mt5545316@gmail.com',
     photoUser: '../assets/images/user3.jpg',
     poster: '../assets/images/portada3.jpeg',
-    password: 123456,
+    password: 'mariat123',
     age: 25,
     contacts: ['pedrocarrillo@gmail.com', 'lauralopez@gmail.com'],
     post: 12
@@ -45,8 +45,5 @@ var dataUsers = [
     text: 'Información importante',
     
   }
-
   ]
-
-
 ];

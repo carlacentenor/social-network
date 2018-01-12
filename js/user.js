a6f897cc1207efa41d4f5f78bd82cea711a5f82a
carlacentenor <carlacentenor@gmail.com>
2018-01-12 18:03:10 -0500

    prueba

--- js/user.js
+++ js/user.js
@@ -144,7 +144,9 @@ $(document).ready(function() {
   // Login con Google
   var provider = new firebase.auth.GoogleAuthProvider();
   $('.btn-google').on('click', function() {
+    event.preventDefault();
     firebase.auth().signInWithPopup(provider).then(function(result) {
+      
       // This gives you a Google Access Token. You can use it to access the Google API.
       var token = result.credential.accessToken;
       // The signed-in user info.

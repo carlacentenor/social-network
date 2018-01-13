43b58cadc3e247d7277bc87dc725430458db81ba
carlacentenor <carlacentenor@gmail.com>
2018-01-12 19:44:57 -0500

    google auth

--- js/user.js
+++ js/user.js
@@ -145,22 +145,47 @@ $(document).ready(function() {
   var provider = new firebase.auth.GoogleAuthProvider();
   $('.btn-google').on('click', function() {
     event.preventDefault();
-    firebase.auth().signInWithPopup(provider).then(function(result) {
-      
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      var token = result.credential.accessToken;
-      // The signed-in user info.
-      var user = result.user;
-      // ...
-    }).catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      // The email of the user's account used.
-      var email = error.email;
-      // The firebase.auth.AuthCredential type that was used.
-      var credential = error.credential;
-      // ...
-    });
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    console.log(user.displayName);
+    console.log(user.photoURL);
+    firebase.database().ref('users/' + user.uid).set({
+      name: user.displayName,
+      email: user.email,
+      uid: user.uid,
+      profilePhoto: user.photoURL
+    }).then(
+      user => {
+        $(location).attr('href', 'home.html');
+      });
+    // ...
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+
+  // Obteniendo datos del usuario actual
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+    // User is signed in.
+      var name = user.displayName;
+      var email = user.email;
+      var photoUrl = user.photoURL;
+      var uid = user.uid;
+
+      $username.text(name);
+      $userEmail.text(email);
+      $profilePhoto.attr('src', photoUrl);
+    } else {
+    // No user is signed in.
+    }
   });
 });

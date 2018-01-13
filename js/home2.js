d8d01a4d98c8688cb3f568f38ae2819d97034f82
carlacentenor <carlacentenor@gmail.com>
2018-01-12 23:34:36 -0500

    user verificadis

--- js/home2.js
+++ js/home2.js
@@ -8,34 +8,48 @@ $(document).ready(function() {
   $('.carousel').carousel();
 
   // Obteniendo datos del usuario actual
-  
+
+
   firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
       // User is signed in.
+      var uid = user.uid;
       var name = user.displayName;
       var email = user.email;
       var photoUrl = user.photoURL;
-      var uid = user.uid;
-
-      $('.user-name').text(name);
-      $('.user-name-post-new').text(name);
-      $('.img-user').attr('src', photoUrl);
-      $('.img-user-post-new').attr('src', photoUrl);
-      $('.img-user-profile').attr('src', photoUrl);
-      $('.email-profile  p').text(email);
+      
+      if (name === null) {
+        var database = firebase.database();
+        var reference = database.ref('users');
+        reference.on('value', function(datos) {
+          users = datos.val();
+          var arrayUser = Object.values(users);
+          for (i = 0; i < arrayUser.length; i++) {
+            if (arrayUser[i].uid === uid) {
+              var id = arrayUser[i];
+                            
+              $('.user-name').text(id.name);
+              $('.user-name-post-new').text(id.name);
+              $('.img-user').attr('src', id.profilePhoto);
+              $('.img-user-post-new').attr('src', id.profilePhoto);
+              $('.img-user-profile').attr('src', id.profilePhoto);
+              $('.user-name-post').text(id.name);
+              $('.email-profile  p').text(id.email);
+            }
+          }
+        }, function(objetoError) {
+          console.log('Error de lectura:' + objetoError.code);
+        });
+      } else {
+        $('.user-name').text(name);
+        $('.user-name-post-new').text(name);
+        $('.img-user').attr('src', photoUrl);
+        $('.img-user-post-new').attr('src', photoUrl);
+        $('.img-user-profile').attr('src', photoUrl);
+        $('.email-profile  p').text(email);
+      }
     } else {
       // No user is signed in.
     }
   });
-
-  /*
-
-      $('.user-name-post-new').text(name);
-      $('.img-user').attr('src', photoUrl);
-      $('.img-user-post-new').attr('src', photoUrl);
-      $('.img-user-profile').attr('src', photoUrl);
-      // $('.poster').css('background-image', 'url("' + posterUser + '")');
-      // $('.background-poster').css('background-image', 'url("' + posterUser + '")');
-     
-  */
 });
